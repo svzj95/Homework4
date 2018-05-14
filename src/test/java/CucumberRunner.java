0d4a64e18e05e490c9a624b0a129e2644e253bfc
svzj95 <svzj95@gmail.com>
2018-05-14 20:52:45 +0300

    Fixed adding plugin

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -3,7 +3,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/"}, glue = {"com/aplana/autotests"})
+@CucumberOptions(features = {"src/test/resources/"}, glue = {"com.aplana.autotests.steps"},
+        plugin={"com.aplana.autotests.utils.AllureReporter"})
 public class CucumberRunner {
 
 }
